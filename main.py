8143803af26125e426a14535530c34f2ab648eb2
RajilSaj <svembe@gmail.com>
2023-05-02 19:08:11 -0400

    WIP

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import requests 
 from bs4 import BeautifulSoup
 
-URL = "https://www.sgg.cg/JO/"
+main = "https://www.sgg.cg/JO/"
 
-def extract_data(url):
-    result = requests.get(URL).text
+def Get_links():
+    result = requests.get(main).text
     doc = BeautifulSoup(result, 'html.parser')
-
     links = doc.find_all('a')
     linked = []
     for a in links:
@@ -14,10 +13,36 @@ def extract_data(url):
         if not (linkis.isalpha()):
             linkin = linkis[:len(linkis) - 1]
             linked.append(linkin)
-            #print(linkin)
-            #print()
-
+    del linked[0:2]
+    del linked[len(linked) - 1 ]
     return linked
 
+def Parse_links():
+    pdf = set()
+    
+    for url in Get_links():
+        new_url = main + url
+        r = requests.get(new_url).text
+        doc = BeautifulSoup(r, 'html.parser')
+        for item in doc.findAll('a'):
+            link = item["href"]
+            if link.endswith('.pdf') :
+                pdf.add(link)
+    print(pdf)
+    return pdf
 
-print(extract_data(URL))
+def Save():
+    for item in Parse_links():
+        print(f"Downloading File: {item[55:]}")
+        r = requests.get(item)
+        with open(f"{item[55:]}", "wb") as f:
+              f.write(r.content)
+    print("done")
+
+
+
+
+
+Get_links()
+Parse_links()
+#Save()
