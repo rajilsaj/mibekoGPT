7ec2549609bf0896089ac174423ed40f66f2d4bc
RajilSaj <svembe@gmail.com>
2023-05-01 23:48:18 -0400

    WIP

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ from bs4 import BeautifulSoup
 
 URL = "https://www.sgg.cg/JO/"
 
-result = requests.get(URL).text
-doc = BeautifulSoup(result, 'html.parser')
-#print(doc.prettify())
+def extract_data(url):
+    result = requests.get(URL).text
+    doc = BeautifulSoup(result, 'html.parser')
 
-links = doc.find_all('a')
-for a in links:
-    print(a.string)
-    print()
+    links = doc.find_all('a')
+    linked = []
+    for a in links:
+        linkis = a.string
+        if not (linkis.isalpha()):
+            linkin = linkis[:len(linkis) - 1]
+            linked.append(linkin)
+            #print(linkin)
+            #print()
+
+    return linked
+
+
+print(extract_data(URL))
